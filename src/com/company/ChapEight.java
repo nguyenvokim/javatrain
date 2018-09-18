11142f2fee79315b59737781b73a7b7322cab553
Vo Kim Nguyen <vo.kim.nguyen@linecorp.com>
2018-09-18 16:40:56 +0700

    Add

--- src/com/company/ChapEight.java
+++ src/com/company/ChapEight.java
@@ -1,4 +1,63 @@
 package com.company;
 
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class ChapEight {
+    public ChapEight() {
+        ArrayList<String> strings = new ArrayList<>(Arrays.asList("string1", "string12", "string131", "string131sss2315", "string15"));
+        ArrayList<String> strings2 = new ArrayList<>(Arrays.asList("xilo1", "xilowawd1", "xilo1dasd", "xilo134234", "xilodasdad1"));
+        OptionalDouble avg = strings.stream()
+                .flatMapToInt((string) -> IntStream.of(string.length()))
+                .average();
+        System.out.println("EX9: Avg string length: " + avg.orElse(0));
+        Optional<String> max10 = strings.stream()
+                .filter(s -> s.length() > 8)
+                .reduce((s, s2) -> s + ", " + s2);
+        System.out.println("EX10:" + max10.orElse(""));
+
+        //EX 13
+        System.out.println("EX13:" + joinMe(Stream.of(strings, strings2)));
+
+        //EX 15
+        IntStream.iterate(0, i -> i + 1)
+                .filter(ChapEight::isPrime)
+                .limit(500)
+                .forEach(System.out::println);
+    }
+
+    /**
+     * EX 12
+     * @param first
+     * @param second
+     * @param <T>
+     * @return
+     */
+    public static <T> Stream<T> zip(Stream<T> first, Stream<T> second) {
+        return Stream.concat(first, second);
+    }
+    private static <T> ArrayList<T> joinMe(Stream<ArrayList<T>> stream) {
+        ArrayList<T> emptyArr = new ArrayList<>();
+        return stream.reduce(emptyArr, (ts, ts2) -> {
+            ts.addAll(ts2);
+            return ts;
+        });
+    }
+    private static <T> ArrayList<T> joinMe2(Stream<ArrayList<T>> stream) {
+        Optional<ArrayList<T>> arrayList = stream.reduce((ts, ts2) -> {
+            ts.addAll(ts2);
+            return ts;
+        });
+        return arrayList.orElse(new ArrayList<T>());
+    }
+    public static boolean isPrime(int n) {
+        if (n % 2==0) return false;
+        for(int i= 3;i * i <= n; i += 2) {
+            if(n % i==0)
+                return false;
+        }
+        return true;
+    }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,38 @@
 package com.company;
 
 import com.company.ChapSix.ChapSiz;
+import javafx.application.Application;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.LongAdder;
 
 public class Main {
-
+    public static Integer sum = 0;
     public static void main(String[] args) {
         //ChapTwo chapTwo = new ChapTwo();
-        ChapThree chapThree = new ChapThree();
+        //ChapThree chapThree = new ChapThree();
         //ChapSiz chapSiz = new ChapSiz();
+//        final LongAdder count = new LongAdder();
+//        count.reset();
+//        ExecutorService executor = Executors.newCachedThreadPool();
+//        System.out.println(count.sum());
+//        for (int i = 1; i < 10; i++) {
+//            executor.execute(() -> {
+//                System.out.println("Start increase");
+//                //Main.sum++;
+//                count.increment();
+//            });
+//        }
+//        try {
+//            Thread.sleep(1000);
+//            throw new InterruptedException();
+//        } catch (InterruptedException ex) {
+//            System.out.println("EXCEADASD");
+//            Thread.currentThread().interrupt();
+//        }
+//        System.out.println(count.sum());
+        //System.out.println(Main.sum);
+        new ChapEight();
     }
 }
